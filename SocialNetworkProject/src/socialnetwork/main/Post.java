6af77d03e29183787d3a33403132ae49c834031d
GeorgiChochov <g.chochov@gmail.com>
2016-08-17 19:38:04 +0300

    Set up bare bones of Post clas.

--- SocialNetworkProject/src/socialnetwork/main/Post.java
+++ SocialNetworkProject/src/socialnetwork/main/Post.java
@@ -6,63 +6,79 @@ import java.util.*;
 import exceptions.InvalidInputException;
 
 public class Post {
-	
+
 	// private constants
 	private static final int MIN_POST_LENGTH = 1;
 	private static final int MAX_POST_LENGTH = 140;
-	
+
 	// private fields
 	private String text;
 	private User poster;
 	private LocalDate dateWhenPosted;
 	private Post question;
-	private List<Post>answers = new ArrayList<Post>();
-	private List<Post>likes = new ArrayList<Post>();
-	
-	
-	
-	
+	private List<Post> answers = new ArrayList<Post>();
+	private List<Post> likes = new ArrayList<Post>();
+	private Set<Hashtag> hashtags = new HashSet<Hashtag>();
+
 	public Post(String text, User poster) throws InvalidInputException {
 		if (Validator.isValidString(text, MIN_POST_LENGTH, MAX_POST_LENGTH))
-		this.text = text;
-		if(poster != null){
+			this.text = text;
+		if (poster != null) {
 			this.poster = poster;
-		}else{
+		} else {
 			throw new InvalidInputException("Not valid poster");
 		}
 		this.dateWhenPosted = dateWhenPosted.now();
-		
+
 	}
-	
-	public String getText(){
+
+	public String getText() {
 		return this.text;
 	}
 
 	public void setQuestion(Post originalPost) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void delete() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void addReply(Post myRetweet) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void reply(Post originalPost) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void retweet(Post originalPost) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
+	public User getPoster() {
+		return poster;
+	}
+
+	public LocalDate getDateWhenPosted() {
+		return dateWhenPosted;
+	}
+
+	public Post getQuestion() {
+		return question;
+	}
+
+	public List<Post> getAnswers() {
+		return answers;
+	}
+
+	public List<Post> getLikes() {
+		return likes;
+	}
 
 }
