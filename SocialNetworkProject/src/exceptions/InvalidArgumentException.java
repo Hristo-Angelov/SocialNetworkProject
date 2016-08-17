d6bae307b8def6deb737c916436191c324ebeb5f
GeorgiChochov <g.chochov@gmail.com>
2016-08-17 15:08:27 +0300

    Test cross-user commit

--- SocialNetworkProject/src/exceptions/InvalidArgumentException.java
+++ SocialNetworkProject/src/exceptions/InvalidArgumentException.java
@@ -6,28 +6,23 @@ public class InvalidArgumentException extends Exception {
 	private static final long serialVersionUID = -5617758274483170018L;
 
 	public InvalidArgumentException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidArgumentException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidArgumentException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidArgumentException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidArgumentException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
